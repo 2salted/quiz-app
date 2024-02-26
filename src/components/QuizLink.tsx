afd158618c9ae6bf3404e67662810cd972231ece
2salted <noamgraf123@gmail.com>
2024-02-26 00:23:22 -0100

    mapped QuizLink to 3 object variables, moved some html to homepage and update the UI

--- src/components/QuizLink.tsx
+++ src/components/QuizLink.tsx
@@ -9,11 +9,11 @@ type Props = {
 export default function QuizLink({ name, link }: Props) {
   return (
     <>
-      <div className="flex flex-col items-center justify-center h-screen">
-        <Link to={link}>
-          <div className="flex flex-col bg-gray-200 p-6 mb-4">{name}</div>
-        </Link>
-      </div>
+      <Link to={link}>
+        <div className="flex flex-col bg-gray-200 p-4 mb-4 w-96 text-center">
+          {name}
+        </div>
+      </Link>
     </>
   );
 }
--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -3,9 +3,22 @@ import { Link } from "react-router-dom";
 import QuizLink from "../components/QuizLink";
 
 export default function Homepage() {
+  let quizData = [
+    { name: "History Test", link: "/quiz" },
+    { name: "Physics Test", link: "/quiz2" },
+    { name: "Math Test", link: "/quiz3" },
+  ];
+
   return (
     <>
-      <QuizLink name={"History Quiz"} link={"/quiz"} />
+      <div className="flex flex-col justify-center items-center h-screen">
+        <h2 className="text-center text-3xl mb-14">
+          Choose a quiz of your choice!
+        </h2>
+        {quizData.map((quiz, index) => (
+          <QuizLink key={index} name={quiz.name} link={quiz.link} />
+        ))}
+      </div>
     </>
   );
 }
