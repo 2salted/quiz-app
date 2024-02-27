5b5bdd523554b75e08f7e30b95c3fec280510154
2salted <noamgraf123@gmail.com>
2024-02-27 18:07:25 -0100

    added functionality (saving before big change)

--- src/pages/Quiz.tsx
+++ src/pages/Quiz.tsx
@@ -7,16 +7,20 @@ export default function Quiz() {
   const { quizId } = useParams();
   const [currentIndexState, setCurrentIndexState] = useState(0);
   let matchedIdText = "";
+  let matchedIdAnswers = [""];
 
   for (let i = 0; i < questions.length; i++) {
     if (quizId !== undefined) {
       let question = questions[i];
       if (question.quizId === parseInt(quizId)) {
         matchedIdText = question.text;
+        matchedIdAnswers = question.answers;
         break;
       }
     }
   }
+  //let IdAnswersString = matchedIdAnswers.toString();
+  //console.log(matchedIdAnswers.length);
 
   function nextQuestion() {
     if (currentIndexState >= 0) {
@@ -35,9 +39,7 @@ export default function Quiz() {
         <div className="bg-white shadow-md rounded-lg w-1/2 border-2 border-blue-600 p-4 mt-16">
           <h3 className="text-center">{matchedIdText}</h3>
           <div className="flex flex-col items-center">
-            {questions.map((answer, index) => (
-              <QuizAnswers key={index} answer={answer.answers} />
-            ))}
+            <QuizAnswers answer={matchedIdAnswers} />
           </div>
         </div>
       </div>
