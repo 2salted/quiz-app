b6dd6eb6aa579fbae0f063137801e447910d41e4
2salted <noamgraf123@gmail.com>
2024-03-19 15:44:15 -0100

    updated useless code

--- src/components/QuizLink.tsx
+++ src/components/QuizLink.tsx
@@ -7,14 +7,12 @@ type Props = {
 
 export default function QuizLink({ name, link }: Props) {
   return (
-    <>
-      <Link to={link}>
-        <div className="pt-2 pb-2">
-          <div className="flex flex-col bg-gray-200 p-4 w-96 text-center">
-            {name}
-          </div>
+    <Link to={link}>
+      <div className="pt-2 pb-2">
+        <div className="flex flex-col bg-gray-200 p-4 w-96 text-center">
+          {name}
         </div>
-      </Link>
-    </>
+      </div>
+    </Link>
   );
 }
